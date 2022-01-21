962a195d52e44a7f5e836bc1f1b5cee351fb0d94
Aiden Moechnig <45131234+qinleader123@users.noreply.github.com>
2022-01-21 13:11:15 -0600

    Delete ecsdbn directory

--- ecsdbn/bar.js
+++
@@ -1,1 +0,0 @@
-function openMenu() {  var navMain = document.getElementById('navMenuId');  if (navMain.className=== 'navMenu') {    navMain.className += ' mobileView';  } else {    navMain.className = 'navMenu';  }}
